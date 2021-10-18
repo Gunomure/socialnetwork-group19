630e5c45820bdab4e24a5c13952a3fb2fe03fce5
Stepan Afanasev <so46@bk.ru>
2021-10-18 19:51:40 +0300

    add entity to BaseEntity

--- domain/src/main/java/ru.skillbox.diplom.model/BaseEntity.java
+++ domain/src/main/java/ru.skillbox.diplom.model/BaseEntity.java
@@ -2,11 +2,13 @@ package ru.skillbox.diplom.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@Entity
 public abstract class BaseEntity {
 
     @Id
