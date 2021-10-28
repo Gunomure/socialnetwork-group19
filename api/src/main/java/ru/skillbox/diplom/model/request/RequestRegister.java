89b985ac12a0e32d7ed0040215cd71605a58ad47
Тимур Воробьев <misikon@mail.ru>
2021-10-28 16:43:30 +0000

    Sprint 1/task 6

---
+++ api/src/main/java/ru/skillbox/diplom/model/request/RequestRegister.java
@@ -0,0 +1,14 @@
+package ru.skillbox.diplom.model.request;
+
+
+import lombok.Data;
+
+@Data
+public class RequestRegister {
+    private String email;
+    private String passwd1;
+    private String passwd2;
+    private String firstName;
+    private String lastName;
+    private String code;
+}
